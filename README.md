91d4c80d25e970e8006f61d53e5e2cd26c904f74
Yaba Ernesto <yabaernesto@gmail.com>
2024-07-16 08:54:43 +0100

    proximo passo na api

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # FinAPI - Financeira
 
-Uma API Rest que desenvolvi durante os meus estudos de NODEJS!
+Uma API Rest que desenvolvi durante os meus estudos de NODEJS na Rocketseat!
 
 ### Requisitos
 - [x] Deve ser possível criar uma conta 
 - [x] Deve ser possível buscar o extrato bancário do cliente
-- [] Deve ser possível realizar um depósito
+- [x] Deve ser possível realizar um depósito
 - [] Deve ser possível realizar um saque
 - [] Deve ser possível buscar o extrato bancário do client por data
 - [] Deve ser possível atualizar dados da conta do cliente
@@ -15,7 +15,7 @@ Uma API Rest que desenvolvi durante os meus estudos de NODEJS!
 ### Regras de negócio
 - [x] Não deve ser possível cadastrar uma conta com o CPF já existente
 - [x] Não deve ser possível fazer depósito em uma conta não existente
-- [] Não deve ser possível buscar extrato em uma conta não existente
+- [x] Não deve ser possível buscar extrato em uma conta não existente
 - [] Não deve ser possível fazer saque em uma conta não existente
 - [] Não deve ser possível excluir uma conta não existente
 - [] Não deve ser possível fazer saque quando o saldo é insuficiente
--- src/index.js
+++ src/index.js
@@ -5,18 +5,34 @@ const app = express();
 
 app.use(express.json());
 
-const custumers = [];
+const customers = [];
+
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  // repassando o objeto que pretendo usar onde o middleware for chamado
+  request.customer = customer;
+
+  return next();
+}
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
-  const customerAlreadyExists = custumers.some((custumer) => custumer.cpf === cpf);
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
 
   if (customerAlreadyExists) {
     return response.status(400).json({ error: "Customer already exists!" });
   }
 
-  custumers.push({
+  customers.push({
     cpf,
     name,
     id: uuidv4(),
@@ -26,16 +42,27 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params;
+app.get("/statement/:cpf", verifyIfExistsAccountCPF, (request, response) => {
+  // pegando o customer que foi armazenado no Middleware
+  const { customer } = request;
+  return response.json(customer);
+});
 
-  const custumer = custumers.find((custumer) => custumer.cpf === cpf);
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
 
-  if (!custumer) {
-    return response.status(400).json({ error: "Customer not found" });
-  }
+  const { customer } = request;
 
-  return response.json(custumer.statement);
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
